5aa3c606de4e76e0233a38b6ed8d0431d7d1ddb6
Sameer Choubey <sameer.choubey98@gmail.com>
2025-03-12 23:33:17 +0100

    404 page + app/(main) skeleton

---
+++ app/(main)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const layout = ({ children }) => {
+    return (
+        <div className="container mx-auto">
+            {children}
+        </div>
+    )
+}
+
+export default layout;
