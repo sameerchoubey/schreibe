95c8b482e288f0997ad25784286e9a1541d2eaf3
Sameer Choubey <sameer.choubey98@gmail.com>
2025-03-16 16:55:20 +0100

    Updated Middleware - shielf and bot protection via ArcJet

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,8 +8,26 @@ const isProtectedRoute = createRouteMatcher([
     "/journal(.*)",
 ])
 
+// Create Arcjet middleware
+const aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    // characteristics: ["userId"], // Track based on Clerk userId
+    rules: [
+        // Shield protection for content and security
+        shield({
+            mode: "LIVE",
+        }),
+        detectBot({
+            mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+            allow: [
+                "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+                // Full list at https://arcjet.com/bot-list
+            ],
+        }),
+    ],
+});
 
-export default clerkMiddleware(async (auth, req) => {
+const clerk = clerkMiddleware(async (auth, req) => {
     const {userId, redirectToSignIn} = await auth();
 
     if(!userId && isProtectedRoute(req)){
@@ -17,6 +36,9 @@ export default clerkMiddleware(async (auth, req) => {
 
     return NextResponse.next();
 });
+
+// Chain middlewares - ArcJet runs first, then Clerk
+export default createMiddleware(aj, clerk);
 
 export const config = {
     matcher: [
